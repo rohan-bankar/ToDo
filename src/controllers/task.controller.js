0e061603457ef8adf22ec0871ac8c22043f6be46
rohan-bankar <rohanbankar011@gmail.com>
2024-04-04 14:17:32 +0530

    few routes added

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -13,12 +13,12 @@ const userTask = asyncHandler(async(req,res)=>{
     if(!content || content.trim() === ""){
         throw new ApiError(400,"Add Task")
     } 
-
+    const userId = req.user._id
     const task = await Task.create({
         content,
-        createdBy: req.user._id
+        createdBy: userId
     })
-
+    
     return res
     .status(200)
     .json(
@@ -44,10 +44,10 @@ const deleteTask =asyncHandler(async(req,res)=>{
     }
 }) 
 
-const deleteAll = asyncHandler(async(req,res)=>{
+const deleteCompleted = asyncHandler(async(req,res)=>{
     const userId = req.user._id
     const oldTask = await Task.deleteMany(
-        {createdBy:userId}
+        {createdBy:userId,completed:true}
     )
     
     if(oldTask){
@@ -82,80 +82,130 @@ const taskDone = asyncHandler(async(req,res)=>{
     }
 })
 
-// const showAllTask = asyncHandler(async(req,res)=>{
-//     const userId = req.user._id
+const showIncompleteTask = asyncHandler(async(req,res)=>{
+    const userId = req.user._id
 
-//     const tasks = await Task.find(
-//         //completed:true or false to display task as per status
-//         {createdBy:userId,completed:false}, 
-//     )
-
-//     if(tasks.length > 0){
-//         return res
-//         .status(200)
-//         .json(
-//             new ApiResponse(200,tasks,"All tasks view")
-//         )
-//     }else{
-//         throw new ApiError(404,"No tasks found for the user") 
-//     }
-// })
-
-const showUserTask = asyncHandler(async (req, res) => {
-    const userId = req.user._id;
-
-    const pipeline = [
-        {
-            $match: {
-                createdBy: new mongoose.Types.ObjectId(userId),
-                completed: false
-            }
-        },
-        {
-            $lookup: {
-                from: 'users',
-                localField: 'createdBy',
-                foreignField: '_id',
-                as: 'user'
-            }
-        },
-        {
-            $unwind: '$user'
-        },
-        {
-            $project: {
-                'user.username': 1,
-                'user.avatar': 1,
-                content: 1,
-                completed: 1
-            }
-        }
-    ];
-
-    const showData = await Task.aggregate(pipeline);
-
-   if(showData.length > 0){
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(200,showData,"User data fetch successfully")
+    const tasks = await Task.find(
+        //completed:true or false to display task as per status
+        {createdBy:userId,completed:false}, 
     )
-   }else{
-    // throw new ApiError(404,"No data found")
-    return res.status(404).json(
-        new ApiResponse(404, null, "No user data found")
-    );
-   }
-   
-});
 
+    const totalCount = await Task.countDocuments(
+        {createdBy:userId,completed:false}
+    )
+
+    if(tasks.length > 0){
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200,tasks,totalCount,"All tasks view")
+        )
+    }else{
+        throw new ApiError(404,"No tasks found for the user") 
+    }
+})
+
+const showCompleteTask = asyncHandler(async(req,res)=>{
+    const userId = req.user._id
+
+    const tasks = await Task.find(
+        //completed:true or false to display task as per status
+        {createdBy:userId,completed:true}, 
+    )
+
+    const totalCount = await Task.countDocuments(
+        {createdBy:userId,completed:true}
+    )
+
+    if(tasks.length > 0){
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200,tasks,totalCount,"All tasks view")
+        )
+    }else{
+        throw new ApiError(404,"No tasks found for the user") 
+    }
+})
+
+// const showUserTask = asyncHandler(async (req, res) => {
+//     const userId = req.user._id;
+
+//     const pipeline = [
+//         {
+//             $match: {
+//                 createdBy: new mongoose.Types.ObjectId(userId),
+//                 // completed: false
+//             }
+//         },
+//         {
+//             $lookup: {
+//                 from: 'users',
+//                 localField: 'createdBy',
+//                 foreignField: '_id',
+//                 as: 'user'
+//             }
+//         },
+//         {
+//             $unwind: '$user'
+//         },
+//         {
+//             $project: {
+//                 'user.username': 1,
+//                 'user.avatar': 1,
+//                 // content: 1,
+//                 // completed: 1
+//             }
+//         }
+//     ];
+
+//     const showData = await Task.aggregate(pipeline);
+
+//    if(showData.length > 0){
+//     return res
+//     .status(200)
+//     .json(
+//         new ApiResponse(200,showData,"User data fetch successfully")
+//     )
+//    }else{
+//     // throw new ApiError(404,"No data found")
+//     return res.status(404).json(
+//         new ApiResponse(404, null, "No user data found")
+//     );
+//    }
+   
+// });
+
+const showAllTask = asyncHandler(async(req,res)=>{
+    const userId = req.user._id
+
+    const tasks = await Task.find(
+        //completed:true or false to display task as per status
+        {createdBy:userId} 
+    )
+    const totalCount = await Task.countDocuments(
+        {createdBy:userId}
+    )
+
+    if(tasks.length > 0){
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200,tasks,totalCount,"All tasks view")
+        )
+    }else{
+        throw new ApiError(404,"No tasks found for the user") 
+    }
+})
 
 
 export{
     userTask,
     deleteTask,
-    deleteAll,
+    deleteCompleted,
     taskDone,
-    // showAllTask
-    showUserTask
+    showIncompleteTask,
+    showCompleteTask,
+    showAllTask
+    // showUserTask
 }
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import {
-    deleteAll,
+    deleteCompleted,
     deleteTask,
-    showUserTask,
-    // showAllTask,
+    showIncompleteTask,
+    showCompleteTask,
+    showAllTask,
+    // showUserTask,
     taskDone,
     userTask
 } from "../controllers/task.controller.js"
@@ -15,9 +17,11 @@ const router = Router()
 // router.use(verifyJWT); // apply verifyJWT middleware to all routes in this file
 router.route("/user-task").post(verifyJWT,userTask)
 router.route("/delete-task").delete(verifyJWT,deleteTask)
-router.route("/delete-all").delete(verifyJWT,deleteAll)
+router.route("/delete-completed").delete(verifyJWT,deleteCompleted)
 router.route("/c/:taskId").patch(verifyJWT,taskDone)
-// router.route("/all-task").get(verifyJWT,showAllTask)
-router.route("/all-task").get(verifyJWT,showUserTask)
+router.route("/incomplete-task").get(verifyJWT,showIncompleteTask)
+router.route("/complete-task").get(verifyJWT,showCompleteTask)
+router.route("/all-task").get(verifyJWT,showAllTask)
+// router.route("/all-task").get(verifyJWT,showUserTask)
 
 export default router
