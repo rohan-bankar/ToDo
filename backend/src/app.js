f24b67d6555beaa4687b79e4f3638ad5e4fb9608
rohan-bankar <rohanbankar011@gmail.com>
2024-04-16 10:21:55 +0530

    cors remove

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 //     credentials:true
 // }))
 
-app.use(cors())
+// app.use(cors())
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
